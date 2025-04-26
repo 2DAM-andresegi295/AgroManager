19580b135f7bb4a6894e042f4f601321e81e8e28
andres <aegifer0806@g.educaand.es>
2025-04-26 11:30:35 +0200

    boton crear parcela y metodo con problemas

--- src/app/pages/add-parcela/add-parcela.page.ts
+++ src/app/pages/add-parcela/add-parcela.page.ts
@@ -1,38 +1,47 @@
 import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
-import { map } from 'rxjs';
 import { MapComponent } from 'src/app/components/map/map.component';
+import { ParcelasService } from 'src/app/services/parcelas/parcelas.service';
 
 @Component({
   selector: 'app-add-parcela',
   templateUrl: './add-parcela.page.html',
   styleUrls: ['./add-parcela.page.scss'],
-  standalone:false,
+  standalone: false,
 })
 export class AddParcelaPage implements OnInit {
   @ViewChild('mapadd') mapadd: MapComponent | undefined;
 
-  vertices: google.maps.LatLngLiteral[]=[{lat: 0, lng: 0}];
-  primero: boolean=false;
-  ultimoClick: google.maps.LatLngLiteral ={lat: 0, lng: 0};
+  vertices: google.maps.LatLngLiteral[] = [{ lat: 0, lng: 0 }];
+  primero: boolean = false;
+  ultimoClick: google.maps.LatLngLiteral = { lat: 0, lng: 0 };
 
-  constructor(private cdRef: ChangeDetectorRef) { }
+  constructor(
+    private cdRef: ChangeDetectorRef,
+    private parcelasService: ParcelasService
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
   onMarcadorCreado(vertice: google.maps.LatLngLiteral) {
-    if(this.primero){
-      this.ultimoClick=vertice;
-    }else{
-      this.vertices[0]=vertice;
-      this.primero=true;
+    if (this.primero) {
+      this.ultimoClick = vertice;
+    } else {
+      this.vertices[0] = vertice;
+      this.primero = true;
     }
     this.cdRef.detectChanges();
   }
   agregarVertice(vertice: google.maps.LatLngLiteral) {
-      this.vertices.push(vertice);
-      console.log(this.vertices.length);
-      this.cdRef.detectChanges();
-      this.mapadd?.guardarMarcador();
-      this.ultimoClick={lat: 0, lng: 0};
+    this.vertices.push(vertice);
+    console.log(this.vertices.length);
+    this.cdRef.detectChanges();
+    this.mapadd?.guardarMarcador();
+    this.ultimoClick = { lat: 0, lng: 0 };
+  }
+  async crearParcela() {
+    try {
+      await this.parcelasService.addParcela('prueba', this.vertices, 'ganado');
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
